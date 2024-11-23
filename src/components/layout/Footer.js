e87e1814b20668d73570478df5eb792d29d3d786
vmgn16 <mg_619@hotmail.com>
2024-11-23 13:43:05 -0600

    pie de pagina y barra de navegacion

---
+++ src/components/layout/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer className="footer">
+      Portafolio Víctor García &copy; {new Date().getFullYear()}
+    </footer>
+  )
+}
